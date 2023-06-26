237839995d594d1be222993c7f431415593e670a
alexdeploy <me@alexrueda.dev>
2023-06-27 00:09:44 +0200

    Added /verify route for verify mail

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { authenticateToken, verifyResetToken } = require('../utils/auth');
-const { loginByMail, registerByMail, registerByUsername, forgotPassword, resetPassword } = require('../controllers/user.controller');
+const { authenticateToken, verifyResetToken } = require('../middleware/auth');
+const { loginByMail, registerByMail, registerByUsername, forgotPassword, resetPassword, verifyMail } = require('../controllers/user.controller');
 
 router.post('/login', loginByMail);
 router.post('/register', registerByMail); // Possible to change to registerByUsername
+router.get('/verify', verifyMail);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password',verifyResetToken, resetPassword);
 
