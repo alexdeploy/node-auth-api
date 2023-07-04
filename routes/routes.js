4b6a1bfd4f15f4d33ae4046c16e5c8c1a674e5fd
alexdeploy <me@alexrueda.dev>
2023-07-04 21:40:50 +0200

    Renamed routes

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { authenticateToken, verifyResetToken } = require('../middleware/auth');
 const { loginByMail, registerByMail, registerByUsername, forgotPassword, resetPassword, verifyMail } = require('../controllers/user.controller');
 
-router.post('/login', loginByMail);
-router.post('/register', registerByMail); // Possible to change to registerByUsername
+router.post('/sign-in', loginByMail);
+router.post('/sign-up', registerByMail); // Possible to change to registerByUsername
 router.get('/verify', verifyMail);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password',verifyResetToken, resetPassword);
