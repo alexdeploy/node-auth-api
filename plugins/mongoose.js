f1a9a65134cfc09488ea6a4f22e54c7c9150788c
alexdeploy <me@alexrueda.dev>
2023-07-29 23:58:54 +0200

    TODO in mongoose

--- plugins/mongoose.js
+++ plugins/mongoose.js
@@ -4,12 +4,16 @@
 const mongoose = require('mongoose');
 const user = process.env.CLUSTER_USER_NAME;
 const password = process.env.CLUSTER_USER_PASSWORD;
+
+// TODO: Ajustar URI dinámica para facil configuración.
 const DB_URI = 'mongodb+srv://' + user + ':' + password + '@cluster0.arhfz8j.mongodb.net/';
+
+// TODO: Investigar sobre las opciones de mongoose.
 const DB_OPTIONS = {
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 }
-
+// TODO: Investigar sobre la respuesta de mongoose y hacer logs enriquecidos.
 mongoose.connect(DB_URI, DB_OPTIONS)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error(err));
