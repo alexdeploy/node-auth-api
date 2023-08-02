2d96d9b7edac2e33f8644af7f00905d5c7c6e2a0
alexdeploy <me@alexrueda.dev>
2023-08-02 17:46:55 +0200

    new and future user model keys

--- models/User.model.js
+++ models/User.model.js
@@ -19,6 +19,18 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
+    images: { // ! Not used
+        avatar: {
+            type: String,
+            required: false,
+            default: null,
+        },
+        cover: {
+            type: String,
+            required: false,
+            default: null,
+        },
+    },
     role: {
         id: {
             type: Number,
@@ -35,7 +47,7 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
-    phone: {
+    phone: { // ! Not used
         prefix: {
             type: String,
             required: false,
@@ -52,24 +64,84 @@ const userSchema = new mongoose.Schema({
             default: false,
         }
     },
-    tokens: {
-        session: {
-            type: String,
-            required: true,
-            default: null,
-        },
-        resetPassword: {
+    address: {
+        street: {
             type: String,
             required: false,
             default: null,
         },
-        verifyEmail: {
+        city: {
             type: String,
             required: false,
             default: null,
-        }
+        },
+        country: {
+            type: String,
+            required: false,
+            default: null,
+        },
+
     },
     security: {
+        // TODO: Implementar nueva seguridad
+        tokens: {
+            session: {
+                type: String,
+                required: true,
+                default: null,
+            },
+            resetPassword: {
+                type: String,
+                required: false,
+                default: null,
+            },
+            verifyEmail: {
+                type: String,
+                required: false,
+                default: null,
+            },
+            verifyPhone: { // ! Not used
+                type: String,
+                required: false,
+                default: null,
+            },
+        },
+        login: { // ! Not used
+            attempts: {
+                type: Number,
+                default: 0,
+            },
+            lastLogin: {
+                type: Date,
+                default: null,
+            },
+            lastLogout: {
+                type: Date,
+                default: null,
+            },
+            lastIp: {
+                type: String,
+                default: null,
+            },
+            lastBrowser: {
+                type: String,
+                default: null,
+            },
+        },
+        locked: { // ! Not used
+            status: {
+                type: Boolean,
+                default: false,
+            },
+            reason: {
+                type: String,
+                default: null,
+            },
+            until: {
+                type: Date,
+                default: null,
+            },
+        },
         lockReason: {
             type: String,
             default: null,
@@ -83,7 +155,7 @@ const userSchema = new mongoose.Schema({
             default: null,
             },
     },
-    settings: {
+    settings: { // ! Not used
         theme: {
             type: String,
             default: null,
