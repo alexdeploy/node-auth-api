1ada3e2918a6a9445edcbfb87cff741565f953cd
alexdeploy <applex0@icloud.com>
2024-09-11 20:20:00 +0200

    mongodb url fix

--- modules/mongodb.js
+++ modules/mongodb.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI)
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
