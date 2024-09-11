06eaaf5efceefe9adeb495b1d67039f5488565d9
alexdeploy <applex0@icloud.com>
2024-09-11 19:56:24 +0200

    mongodb railway connection

--- plugins/mongoose.js
+++ plugins/mongoose.js
@@ -2,9 +2,7 @@
  * Mongoose Documentation API - http://mongoosejs.com/docs/api.html
  */
 const mongoose = require('mongoose');
-const user = process.env.CLUSTER_USER_NAME;
-const password = process.env.CLUSTER_USER_PASSWORD;
-const DB_URI = 'mongodb+srv://' + user + ':' + password + '@cluster0.arhfz8j.mongodb.net/';
+const DB_URI = process.env.DB_MONGO_URI;
 const DB_OPTIONS = {
     useNewUrlParser: true, 
     useUnifiedTopology: true 
